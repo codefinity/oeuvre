80a872f4add6a877cb3a4d003a430a108ddd798e
Nikhil P <nikhilpat@gmail.com>
2020-08-11 12:43:56 +0530

    Initial Integration Test

---
+++ server/src/Tests/Oeuvre.API.IntegrationTests/IdentityAccess/RegistrationIntegrationTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace Oeuvre.API.IntegrationTests.IdentityAccess
+{
+    public class RegistrationIntegrationTests : IClassFixture<OeuvreTestFixture>
+    {
+        //private readonly OeuvreTestFixture fixture;
+        private readonly HttpClient client;
+        public RegistrationIntegrationTests(OeuvreTestFixture fixture)
+        {
+            //this.fixture = fixture;
+            this.client = fixture.CreateClient();
+        }
+
+
+        [Theory]
+        [InlineData("/identityaccess/registrants")]
+        public async void Get_GetAllRegistrants_Valid_Success(string url)
+        {
+            // Act
+            var response = await client.GetAsync(url);
+
+            var contents = await response.Content.ReadAsStringAsync();
+
+
+            // Assert1
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            // Assert2
+            Assert.NotEmpty(contents);
+            // Assert2
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+
+}
---
+++ server/src/Tests/Oeuvre.API.IntegrationTests/OeuvreTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oeuvre.API.IntegrationTests
+{
+    public class OeuvreTestFixture : WebApplicationFactory<Startup>
+    {
+        //override methods here as needed for Test purpose
+    }
+}
