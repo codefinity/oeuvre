03aea30d19ea981d1d95f259fd3b1991db1384ad
Nikhil P <nikhilpat@gmail.com>
2020-09-10 11:41:44 +0530

    Updated features

--- server/src/Specifications/Features/Functional/IdentityAccess/Oeuvre.Specs.IdentityAccess/Features/UserSettings.feature
+++ server/src/Specifications/Features/Functional/IdentityAccess/Oeuvre.Specs.IdentityAccess/Features/UserSettings.feature
@@ -3,4 +3,5 @@
 Scenario: Member can update his Settings
 	Given I am a Registered User
 	When I try to update my Settings
-	Then My user Settings should be updated
+	Then My user Settings should be updated 
+	 
--- server/src/Specifications/Features/Functional/IdentityAccess/Oeuvre.Specs.IdentityAccess/Steps/RegistrationSteps.cs
+++ server/src/Specifications/Features/Functional/IdentityAccess/Oeuvre.Specs.IdentityAccess/Steps/RegistrationSteps.cs
@@ -6,28 +6,28 @@ namespace Oeuvre.Specs.IdentityAccess.Steps
     [Binding]
     public class RegistrationSteps
     {
-        [Given(@"I am a new user of the product")]
-        public void GivenIAmANewUserOfTheProduct()
+        [Given(@"I am not a User of Oeuvre")]
+        public void GivenIAmNotAUserOfOeuvre()
         {
-            ScenarioContext.Current.Pending();
+            //ScenarioContext.Current.Pending();
         }
         
-        [When(@"I register the following valid details")]
-        public void WhenIRegisterTheFollowingValidDetails(Table table)
+        [When(@"I register the following invalid details")]
+        public void WhenIRegisterTheFollowingInvalidDetails(Table table)
         {
-            ScenarioContext.Current.Pending();
+            //ScenarioContext.Current.Pending();
         }
         
-        [Then(@"a new account will be created for me")]
-        public void ThenANewAccountWillBeCreatedForMe()
+        [Then(@"a new account will not be created for me")]
+        public void ThenANewAccountWillNotBeCreatedForMe()
         {
-            ScenarioContext.Current.Pending();
+            //ScenarioContext.Current.Pending();
         }
         
-        [Then(@"I should receive a registration mail containing the email verification link account")]
-        public void ThenIShouldReceiveARegistrationMailContainingTheEmailVerificationLinkAccount()
+        [Then(@"I should not receive a registration mail containing the email verification link account")]
+        public void ThenIShouldNotReceiveARegistrationMailContainingTheEmailVerificationLinkAccount()
         {
-            ScenarioContext.Current.Pending();
+            //ScenarioContext.Current.Pending();
         }
     }
 }
