08422cc99451b3cbb08c3cbc6aea0c8571ce9af3
Nikhil P <nikhilpat@gmail.com>
2020-09-11 00:31:35 +0530

    Created batch file to run all tests

---
+++ run-all-tests.bat
@@ -0,0 +1,11 @@
+::Integration Tests
+
+dotnet test server\src\Tests\Oeuvre.API.IntegrationTests\Oeuvre.API.IntegrationTests.csproj
+
+::Running all tests for IdentityAccessModule
+
+dotnet test server\src\Modules\IdentityAccess\Tests\Oeuvre.Modules.IdentityAccess.Domain.UnitTests\Oeuvre.Modules.IdentityAccess.Domain.UnitTests.csproj
+
+dotnet test server\src\Modules\IdentityAccess\Tests\Oeuvre.Modules.IdentityAccess.IntegrationTests\Oeuvre.Modules.IdentityAccess.IntegrationTests.csproj
+
+dotnet test server\src\Modules\IdentityAccess\Tests\Oeuvre.Modules.IdentityAccess.ArchTests\Oeuvre.Modules.IdentityAccess.ArchTests.csproj
