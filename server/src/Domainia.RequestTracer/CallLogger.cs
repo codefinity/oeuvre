d404b7cf05c979cb9a1ff1f4044ad5dcd9ad9405
Nikhil P <nikhilpat@gmail.com>
2020-09-27 12:17:54 +0530

    Added a library for requet tracing purposes

---
+++ server/src/Domainia.RequestTracer/CallLogger.cs
@@ -0,0 +1,30 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Domainia.RequestTracer
+{
+    public class CallLogger : IInterceptor
+    {
+        TextWriter _output;
+
+        public CallLogger(TextWriter output)
+        {
+            _output = output;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            _output.Write("Calling method {0} with parameters {1}... ",
+              invocation.Method.Name,
+              string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray()));
+
+            invocation.Proceed();
+
+            _output.WriteLine("Done: result was {0}.", invocation.ReturnValue);
+        }
+    }
+}
